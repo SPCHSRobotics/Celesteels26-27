05f73d43ca6a2fce7700126aa57ae5863dfd9fc8
AinsleyLaw <ainsleylawr07@gmail.com>
2026-05-08 14:56:49 -0400

    Add code to ainsleySandbox

--- TeamCode/src/main/java/org/firstinspires/ftc/sandbox/ainsley/test1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/sandbox/ainsley/test1.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.sandbox.ainsley;
 
-public class test1 {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="ainsleyTeleOp")
+public class test1 extends OpMode {
+    DcMotor rfMotor;
+    DcMotor rbMotor;
+    public void init() {
+        rfMotor = hardwareMap.get(DcMotor.class, "rfMotor");
+
+    }
+
+    public void loop() {
+        rfMotor.setPower(1);
+    }
+
 }
